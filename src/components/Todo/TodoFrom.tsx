9de7602f6c3f2f20213ebd9f3d55a05d2ed68391
Yeakub1 <mryeakub10@gmail.com>
2024-01-10 12:20:21 +0600

    code update

--- src/components/Todo/TodoFrom.tsx
+++ src/components/Todo/TodoFrom.tsx
@@ -1,12 +1,41 @@
-import { useContext } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { todoContext } from "../../context/TodoProvider";
 
 const TodoFrom = () => {
-    const { totoTitle } = useContext(todoContext);
-    return (
-        <div>
-            <h1>This is todo Component</h1>
-        </div>
-    );
+  const { state, dispatch } = useContext(todoContext);
+  const [task, setTask] = useState("");
+  console.log(state);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random().toString(36).substring(2, 7),
+      title: task,
+      isCompelete: false,
+    };
+    dispatch({ type: "addTodo", payload: todo });
+  };
+
+  return (
+    <div>
+      <h1>Add Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Todo">Task</label>
+        <input
+          className="border border-red-800 "
+          type="text"
+          name="todo"
+          id="todo"
+          onBlur={(e) => setTask(e.target.value)}
+        />
+        <input
+          className="btn bg-purple-800 px-6 py-2 text-white"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
+  );
 };
 export default TodoFrom;
